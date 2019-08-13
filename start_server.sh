7415e1094047c3fded42e8d987fba236540c9c82
Niraj Menon <nirajmmenon@gmail.com>
2019-08-13 16:02:45 +0000

    Added path corrections and apache config file

--- start_server.sh
+++ start_server.sh
@@ -6,26 +6,26 @@ then
     echo "Usage: "
     echo "start_server [main|dev]"
 else
-    cd ~/nodejs_cvc
+    cd ~/270sim
     time="$(date "+%F-%T")"
     if [[ "$1" = "main" ]]
     then
 	pkill -9 cvc
-        ps axf | grep "node /home/menon18/nodejs_cvc/js_cvc_interface_prod.js" | grep -v grep | awk '{print "kill -9 " $1}' | sh
+        ps axf | grep "node /home/menon18/270sim/js_cvc_interface_prod.js" | grep -v grep | awk '{print "kill -9 " $1}' | sh
         echo "Starting main server at $time"
-        node ~/nodejs_cvc/js_cvc_interface_prod.js 2>&1 > ~/nodejs_cvc/logging/main_"$time" &
-	alias nanomainlog="nano ~/nodejs_cvc/logging/main_$time"
-	alias tailmainlog="tail -f ~/nodejs_cvc/logging/main_$time"
-	printf "alias nanomainlog=\"nano ~/nodejs_cvc/logging/main_$time\"\nalias tailmainlog=\"tail -f ~/nodejs_cvc/logging/main_$time\"\n" > sourcelogs
+        node ~/270sim/js_cvc_interface_prod.js 2>&1 > ~/270sim/logging/main_"$time" &
+	alias nanomainlog="nano ~/270sim/logging/main_$time"
+	alias tailmainlog="tail -f ~/270sim/logging/main_$time"
+	printf "alias nanomainlog=\"nano ~/270sim/logging/main_$time\"\nalias tailmainlog=\"tail -f ~/270sim/logging/main_$time\"\n" > sourcelogs
     else if [[ "$1" = "dev" ]] 
          then
    	    pkill -9 cvc
-	    ps axf | grep "node /home/menon18/nodejs_cvc/js_cvc_interface.js" | grep -v grep | awk '{print "kill -9 " $1}' | sh
+	    ps axf | grep "node /home/menon18/270sim/js_cvc_interface.js" | grep -v grep | awk '{print "kill -9 " $1}' | sh
             echo "Starting dev server at $time"
-            node ~/nodejs_cvc/js_cvc_interface.js 2>&1 > ~/nodejs_cvc/logging/dev_"$time" &
-    	    alias nanodevlog="nano ~/nodejs_cvc/logging/dev_$time"
-    	    alias taildevlog="tail -f ~/nodejs_cvc/logging/dev_$time"
-	    printf "alias nanodevlog=\"nano ~/nodejs_cvc/logging/dev_$time\"\nalias taildevlog=\"tail -f ~/nodejs_cvc/logging/dev_$time\"\n" > sourcelogs
+            node ~/270sim/js_cvc_interface.js 2>&1 > ~/270sim/logging/dev_"$time" &
+    	    alias nanodevlog="nano ~/270sim/logging/dev_$time"
+    	    alias taildevlog="tail -f ~/270sim/logging/dev_$time"
+	    printf "alias nanodevlog=\"nano ~/270sim/logging/dev_$time\"\nalias taildevlog=\"tail -f ~/270sim/logging/dev_$time\"\n" > sourcelogs
          fi
     fi
     cd - > /dev/null
